00653817f52a5ad43311d27d970876d7dc9c27c4
Denis Dombrovskii <denis33722@yandex.ru>
2022-08-23 22:10:40 +0300

    make task8

---
+++ seminar_1/task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число для которого вы хотите получить все четные числа: ");
+int num = int.Parse(Console.ReadLine());
+int current = 1;
+
+while (current <= num)
+{
+    if(current % 2 ==0)
+    Console.Write(current + " ");
+    current++;
+}
